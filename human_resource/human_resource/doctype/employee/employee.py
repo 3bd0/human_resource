a24c6c6a4fc5e4bab1b2f1fa815842ccc8dff03b
Abdalkarim Shehab <abdo.r.shehab2@gmail.com>
2023-01-22 21:50:51 +0200

    New Doctype Leave Application & Allocation

--- human_resource/human_resource/doctype/employee/employee.py
+++ human_resource/human_resource/doctype/employee/employee.py
@@ -1,8 +1,13 @@
 # Copyright (c) 2023, me and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from datetime import time
 
 class Employee(Document):
 	pass
+	# def validate(self):
+	# 	self.bod = self.date_of_birth
+	# 	self.cday = time.
+	# 	frappe.msgprint(self.bod)
